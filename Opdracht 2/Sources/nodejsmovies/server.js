3341560a23762ca92ca5eb2c287ba60aca715494
Renzo Veldkamp <renzo.veldkamp@centric.eu>
2019-05-27 09:14:05 +0200

    fixed vulnerabilities and serverjs function

--- Opdracht 2/Sources/nodejsmovies/server.js
+++ Opdracht 2/Sources/nodejsmovies/server.js
@@ -4,6 +4,6 @@ var app = express();
 var movies = require('./movies');
 app.use('/api/movies', movies);
 
-app.listen(process.env.PORT || 80, function () {
-    console.info('The server is listening at port ' + (process.env.PORT || 80));
+app.listen(process.env.PORT || 8080, function () {
+    console.info('The server is listening at port ' + (process.env.PORT || 8080));
 });
